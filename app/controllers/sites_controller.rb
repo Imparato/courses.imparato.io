6bdf722dab496564d58f778a3b10f8c36ad257cf
HugoBayle <h.bayle@hotmail.fr>
2019-07-17 11:56:12 +0200

    commit

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,12 +2,15 @@
 
 class SitesController < ApplicationController
   # before_action :retrieve_site_url, only: [:companies_in_city, :find_city_name_in_url]
+
   def show
     # domain name to delete
     @domain_name = "coursdetheatremarseille.com"
     @site = Site.find_by(domain_name: @domain_name)
     @companies = Company.where(site: @site)
     @cross_pages = CrossPage.where(site: @site, active: true)
+    markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+    @description = markdown.render("This is *bongos*, indeed.")
   end
 
   def retrieve_site_url
