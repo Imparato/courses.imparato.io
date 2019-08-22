cadc7fd8744d65b2a974683abdc9d3a5546fbc54
Ana Negura <ana_n21@yahoo.com>
2019-08-22 22:33:25 +0200

    view if some contact missing

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -3,14 +3,11 @@
 class SitesController < ApplicationController
   # before_action :retrieve_site_url, only: [:companies_in_city, :find_city_name_in_url]
   def show
-    # domain name to delete
+    # domain name to delete before action and retrieve url to uncoment
     @domain_name = "coursdetheatremarseille.com"
     @site = Site.find_by(domain_name: @domain_name)
     @companies = Company.where(site: @site, active: true).page params[:page]
     @cross_pages = CrossPage.where(site: @site, active: true)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
-    :autolink => true, :space_after_headers => true)
-    @description = markdown.render("This is *bongos*, indeed.").html_safe
   end
 
   def retrieve_site_url
