95e26a23139cce11aab9228d609df41beb9df817
Ana Negura <ana_n21@yahoo.com>
2019-08-22 21:21:06 +0200

    validates at least one required

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,8 +4,14 @@ class Company < ApplicationRecord
   belongs_to :site
   has_and_belongs_to_many :tags
   validates :name, :address, :description, presence: true
+  validate :validate_contact
 
   paginates_per 5
   mount_uploader :picture, PictureUploader
   # need to add validation on at least one field phone, mail or website
+  def validate_contact
+    if phone.blank? && mail.blank? && website.blank?
+      errors.add(:mail, "You must fill in at least one contact field")
+    end
+  end
 end
