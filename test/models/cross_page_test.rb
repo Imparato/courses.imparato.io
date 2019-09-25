11421db6c4e90fd0496aa25704cab415f334bd30
Nicolas Minguet <nicolas.minguet@hotmail.fr>
2019-09-25 13:25:48 +0200

    adding Accueil button

--- test/models/cross_page_test.rb
+++ test/models/cross_page_test.rb
@@ -4,7 +4,6 @@ class CrossPageTest < ApplicationSystemTestCase
 
   test "visiting the cross_pages and the home button" do
     impro_tag = FactoryBot.create(:tag)
-    # binding.pry
     impro_crosspage = FactoryBot.create(:cross_page)
     SitesController.any_instance.stubs(:find_domain_from_url).returns(Site.first.domain_name)
     impro_crosspage.tags = [impro_tag]
@@ -13,7 +12,7 @@ class CrossPageTest < ApplicationSystemTestCase
     click_on "IMPRO"
     save_and_open_page
     assert_equal "/impro-a-marseille", page.current_path
-    click_button('Accueil')
+    click_on 'Accueil'
     assert_equal root_path, page.current_path
     save_and_open_page
   end
