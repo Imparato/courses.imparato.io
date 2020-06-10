ec4d5b785aec9c23f4f089b02e72f57910b6c760
Colin Darie <colin@darie.eu>
2020-06-10 11:23:39 +0200

    Test: use selenium instead of headless chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 # test/application_system_test_case.rb
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :headless_chrome # Update this line
+  driven_by :selenium_headless
 end
