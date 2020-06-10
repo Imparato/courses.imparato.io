9c254e32989689eef566f04b7a0c351ed36977ba
Colin Darie <colin@darie.eu>
2020-06-10 18:28:45 +0200

    Deploy: link shared dirs for puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, "/home/courses/#{fetch(:application)}/#{fetch(:stage)}"
 append :linked_files, ".env", "config/master.key"
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/uploads"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
